2f2a53a5b58b5dfb4b15c503b0233bacd6187e7b
smidovic <msmidovic@gmail.com>
2021-11-07 19:37:13 +0100

    farma

--- Open-Lab-02.04/Farm.cs
+++ Open-Lab-02.04/Farm.cs
@@ -6,7 +6,11 @@ namespace Open_Lab_02._04
     {
         public int GetLegsCount(int chickens, int cows, int pigs)
         {
-            throw new NotImplementedException();
+           int chickenCount = chickens * 2;
+           int cowsCount = cows * 4;
+           int pigsCount = pigs * 4;
+           return chickenCount + cowsCount + pigsCount;
+
         }
     }
 }
